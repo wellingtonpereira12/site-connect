60077178a639132d18362a3c88af48e7cb57eab8
Wellington Pereira <lleco12@gmail.com>
2024-09-16 16:19:23 -0300

    feat

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from 'react';
+import logoConnect from '../../assets/logoConnect.png'
 import './Navbar.css'
 
 const Navbar = () => {
     const [active, setActive] = useState("nav_menu");
     const [toggleIcon, setToggleIcon] = useState("nav_toggler");
+    const [scrolled, setScrolled] = useState(false);
 
     const navToggle =() =>{
         active === "nav_menu"
@@ -14,16 +16,32 @@ const Navbar = () => {
         ? setToggleIcon('nav_toggler toggler')
         : setToggleIcon('nav_toggler')
     }
- 
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const isScrolled = window.scrollY > 50;
+          if (isScrolled !== scrolled) setScrolled(isScrolled);
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [scrolled]);
+    
     return(
         <div className="navbar">
-            <nav className={`nav`}>
-                <a href="#" className="nav_brand">Connect</a>
+            <nav className={`nav  ${scrolled ? 'scrolled' : ''}`}>
+                <div className='containerlogo'>
+                    <img src={logoConnect} alt="logoConnect" className={`logo ${scrolled ? 'scrolled' : ''}`}/>
+                    <div className={`ContainerNome ${scrolled ? 'scrolled' : ''}`}>
+                        <a href="#" className={`nomeEmpresa ${scrolled ? 'scrolled' : ''}`} >Connect</a>
+                        <a href="#" className={`DescrEmpresa ${scrolled ? 'scrolled' : ''}`} >Desenvolvimento Web</a>
+                    </div>
+                </div>
                 <ul className={active}>
-                    <li  className="nav_item" ><a href="#inicio" className="nav_link">Início</a></li>
-                    <li  className="nav_item" ><a href="#servico" className="nav_link">Serviço</a></li>
-                    <li  className="nav_item" ><a href="#sobre-nos" className="nav_link">Sobre Nós</a></li>
-                    <li  className="nav_item" ><a href="#duvidas" className="nav_link">Dúvidas</a></li>
+                    <li><a className={`a ${scrolled ? 'scrolled' : ''}`} href="#inicio">Início</a></li>
+                    <li><a className={`a ${scrolled ? 'scrolled' : ''}`} href="#servico">Serviço</a></li>
+                    <li><a className={`a ${scrolled ? 'scrolled' : ''}`} href="#sobre-nos">Sobre Nós</a></li>
+                    <li><a className={`a ${scrolled ? 'scrolled' : ''}`} href="#duvidas">Dúvidas</a></li>
                 </ul>
                 <div onClick={navToggle} className={toggleIcon}>
                    <div className="line1"></div>
--- src/Components/Section/Sobre/Sobre.jsx
+++ src/Components/Section/Sobre/Sobre.jsx
@@ -4,9 +4,9 @@ import '../Section.css';
 function Sobre() {
     return (
         <section id="sobre-nos" className="section">
-         <h1 className="section-title">sobre</h1>
-         <p>Conteúdo da seção sobre.</p>
-      </section>
+            <h1 className="section-title">sobre</h1>
+            <p>Conteúdo da seção sobre.</p>
+        </section>
     )
 }
 
