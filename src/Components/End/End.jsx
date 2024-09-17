7e8c29f10e8ed9e09c2d4675d19235f9d5b7846c
Wellington Pereira <lleco12@gmail.com>
2024-09-17 20:19:34 -0300

    dev: end

--- src/Components/End/End.jsx
+++ src/Components/End/End.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import './End.css'
+import logo from '../../assets/ReactLogo.png'
 
 function End() {
+
+    const handleClick = () => {
+        window.open('https://api.whatsapp.com/send/?phone=47984862476', '_blank');
+    };
     return (
         <div className="endSectionEnd">
-            <div className="endSectionEndEmail">
-                <div>
-                    <h1>Interessado em Construir seu Site?</h1>
-                    <div>wellingtonpereira@gmail.com</div>
-                </div>
-                <button>Enviar</button>
+            <div className="endSectionEndParticao">
+                <h1>Interessado em Construir seu Site?</h1>
+                <button onClick={handleClick}><div className="endflecha-esquerda" /></button>
             </div>
-            <div>oi</div>
+            <div className="endContainerPrincipal">
+            <div className="endlinha-branca" />
+            <div className="endContainer">
+                <div className="endDivLogo">
+                    <img src={logo} alt="ReactLogo" className="endlogo"/>
+                    <h1>React Soluções</h1>
+                </div>
+                <div className="endAteLogo">Até logo! Nos procure se quando quiser!</div>
+                <div className="endDado">
+                    <div className="">Blumenau, Santa Catarina</div>
+                    <div className="">(47) 98486-2476</div>
+                </div>
+            </div>
+            <div className="endContainer1">
+                <div className="endDivLogo1">
+                    <h1>Explorar</h1>
+                </div>
+                <div className="endDado1">
+                    <div>Inicio</div>
+                    <div>Serviços</div>
+                    <div>Sobre Nós</div>
+                    <div>Dúvidas</div>
+                </div>
+            </div>
+            </div>
+            <div className="endDireitos">2024 - React Soluções - Todos os direitos reservados</div>
         </div>
     )
 }
