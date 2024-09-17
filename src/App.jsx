64ad29d007fe4c9ca6ea55114960510cece69f0b
Wellington Pereira <lleco12@gmail.com>
2024-09-17 02:40:38 -0300

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Servico from './Components/Section/Servico/Servico.jsx';
 import Duvida from './Components/Section/Duvidas/Duvidas.jsx';
 import Sobre from './Components/Section/Sobre/Sobre.jsx';
 import End from './Components/End/End.jsx';
-import './app.css';
 
 function App() {
   return (
