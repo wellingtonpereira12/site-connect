b5ae2f6c92badb86733c628675d6f0ba991b3392
Wellington Pereira <lleco12@gmail.com>
2024-09-17 16:30:28 -0300

    dev: Alteração no logo e inicio end

--- src/Components/End/End.jsx
+++ src/Components/End/End.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import './end.css'
 
 function End() {
     return (
-        <div>oi</div>
+        <div className="endSectionEnd">
+            <div className="endSectionEndEmail">
+                <div>
+                    <h1>Interessado em Construir seu Site?</h1>
+                    <div>wellingtonpereira@gmail.com</div>
+                </div>
+                <button>Enviar</button>
+            </div>
+            <div>oi</div>
+        </div>
     )
 }
 
