0a5b56ca77679c05cfb324ed6bd3797530226909
Wellington Pereira <lleco12@gmail.com>
2024-09-21 23:04:21 -0300

    Melhoria botão whats

--- src/Components/Whats/Whats.jsx
+++ src/Components/Whats/Whats.jsx
@@ -1,9 +1,8 @@
-
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 
 const Whats = () => {
-  const phoneNumber = '47984862476'; // Coloque o número de telefone com DDD
+  const phoneNumber = '47984862476';
   const message = 'Olá,%20gostaria%20de%20saber%20mais%20sobre%20como%20sua%20empresa%20pode%20me%20ajudar%20a%20criar%20um%20site%20personalizado.%20Podemos%20conversar%20sobre%20os%20detalhes%20e%20etapas%20do%20projeto?';
 
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
@@ -13,6 +12,7 @@ const Whats = () => {
       href={whatsappUrl}
       target="_blank"
       rel="noopener noreferrer"
+      aria-label="Contate-nos pelo WhatsApp"
       style={{
         position: 'fixed',
         width: '60px',
@@ -29,6 +29,7 @@ const Whats = () => {
       }}
     >
       <i className="fa fa-whatsapp" style={{ marginTop: '16px' }} />
+      <span style={{ display: 'none' }}>Contate-nos pelo WhatsApp</span>
     </a>
   );
 };
