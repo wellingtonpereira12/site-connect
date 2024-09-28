57f0dccb5fb53f1ded6ef6ccee3a70fccf6b13c9
Wellington Pereira <lleco12@gmail.com>
2024-09-28 17:13:26 -0300

    Botão do Whats

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from 'react';
 import logo from '../../assets/ReactLogo.webp'
 import './Navbar.css'
 import { Instagram, Menu, SquareX} from 'lucide-react';
+import { FaWhatsapp } from "react-icons/fa";
 
 
 const Navbar = () => {
     const [active, setActive] = useState("nav_menu");
     const [toggleIcon, setToggleIcon] = useState("nav_toggler");
     const [scrolled, setScrolled] = useState(false);
-
+    const phoneNumber = '47984862476'; // Coloque o número de telefone com DDD
+    const message = 'Olá,%20gostaria%20de%20de%20criar%20uma%20prévia%20grátis%20da%20minha%20marca.%20Podemos%20conversar%20sobre%20os%20detalhes%20e%20etapas%20do%20projeto?';
+  
     const handleClickInsta = () => {
         window.open(`https://www.instagram.com/reactsolucoes/`, '_blank');
+    };
+
+    const handleClickWhatsapp = () => {
+            window.open(`https://wa.me/${phoneNumber}?text=${message}`, '_blank');
     };
 
     const navToggle =() =>{
@@ -48,8 +55,17 @@ const Navbar = () => {
                     <li><a className={`a ${scrolled ? 'scrolled' : ''}`} href="#servico">Serviço</a></li>
                     <li><a className={`a ${scrolled ? 'scrolled' : ''}`} href="#sobre-nos">Sobre Nós</a></li>
                     <li><a className={`a ${scrolled ? 'scrolled' : ''}`} href="#duvidas">Dúvidas</a></li>
-                    <button onClick={handleClickInsta} className={`btnInsta ${scrolled ? 'scrolled' : ''}`}> 
-                        <Instagram /> Instagram
+                    <button onClick={handleClickWhatsapp} className={`btnNav ${scrolled ? 'scrolled' : ''}`}> 
+                        <FaWhatsapp className='FaWhatsapp'/>
+                        {toggleIcon !== 'nav_toggler' ? (
+                            <div>Whatsapp</div>
+                        ) : null}
+                    </button>
+                    <button onClick={handleClickInsta} className={`btnNav ${scrolled ? 'scrolled' : ''}`}> 
+                        <Instagram /> 
+                        {toggleIcon !== 'nav_toggler' ? (
+                            <div>Instagram</div>
+                        ) : null}
                     </button>
                 </ul>
                 <div onClick={navToggle} className={toggleIcon}>
